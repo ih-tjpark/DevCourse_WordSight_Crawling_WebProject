291616a7d113c5d827739a03249a863facaecc10
Dahye K <kangdiamond@gmail.com>
2023-05-03 19:04:01 +0900

    add error object update urls

--- WordSightProject/wordsight/wordsight_front/urls.py
+++ WordSightProject/wordsight/wordsight_front/urls.py
@@ -6,8 +6,9 @@ app_name = 'WORDSIGHT'
 
 urlpatterns = [
     path('',views.index, name="index"),
-    path("<str:keyword>/", views.insight, name="insight"),
-    path("?search=<str:keyword>/", views.insight, name="insight"),
+    path("more/", views.more, name="more"),
+    path("search/", views.search, name="search"),
     path("<uuid:news_id>/", views.detail, name="detail"),
-    path("more-news/", views.more, name="more")
+    
 ]
+
