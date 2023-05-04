a01e2299f0f2cbe6942153eb77f18c026b651863
TJ Park <hahxowns@naver.com>
2023-05-05 02:55:39 +0900

    Update model and Create keyword_m2m
    model update for keyword_m2m
    
    keyword_m2m = 뉴스와 키워드 관계위해 만든 자동화 코드

--- WordSightProject/wordsight/new_api/fixtures/news_loder.py
+++ WordSightProject/wordsight/new_api/fixtures/news_loder.py
@@ -1,8 +1,16 @@
 import json 
 import re
-with open('new_api/fixtures/news_data.json', 'r', encoding='UTF-8') as f:
+with open('crawling_data_real_final.json', 'r', encoding='UTF-8') as f:
     news_list = json.load(f)
 # print(news_list)
+
+def tag_reg(tag):
+    tag = news["tag"]
+    tag = tag.replace(' ','')
+    tag = tag.replace('>',' ')
+    tag = tag.replace('|',' ')
+    tag = tag.split()
+    return tag
 
 new_list = []
 for news in news_list:
@@ -11,15 +19,17 @@ for news in news_list:
     new_data["fields"]["title"] = news["title"]
     new_data["fields"]["news_agency"] = news["news_agency"]
     new_data["fields"]["origin_address"] = news["origin_url"]
-    new_data["fields"]["tag"] = ",".join(set(re.split(r'\s+', re.sub(r'[,\-?>|]',' ', news["tag"]))))
+    new_data["fields"]["tag_list"] = json.dumps(tag)
     new_data["fields"]["created_date"] = news["created_date"]
     # 이름, 이름1,이름2,이름3, 이름~~@.com, JTV 등등 유형이 다양함 ->      
     new_data["fields"]["reporter"] = news["reporter"]
     new_data["fields"]["image_link"] = news["image"]
     new_data["fields"]["news_contents"] = news["news_contents"]
+    new_data["fields"]['keyword_list'] = json.dumps(news['keyword'])
+
 
     new_list.append(new_data)
 
 
-with open('news_data_test.json', 'w', encoding='UTF-8') as f:
+with open('news_data_result.json', 'w', encoding='UTF-8') as f:
     json.dump(new_list, f, ensure_ascii=False, indent=2)
