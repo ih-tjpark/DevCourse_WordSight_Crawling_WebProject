d95c2feed1f9f8439e7dea263c671995747b7541
yunji jo <dbtmxlsk2007@naver.com>
2023-05-04 21:33:39 +0900

    add crawling data and keyword load file
    add in baek-end

---
+++ WordSightProject/wordsight/new_api/fixtures/keyword_loader.py
@@ -0,0 +1,17 @@
+import json 
+import re
+with open('new_api/fixtures/keyword_data_final3.json', 'r', encoding='UTF-8') as f:
+    keyword_list = json.load(f)
+
+new_list = []
+for keyword in keyword_list:
+    new_data = {"model": "new_api.keyword"}
+    new_data["fields"] = {}
+    new_data["fields"]['name'] = keyword
+    new_data['fields']['count'] = keyword_list[keyword]['count']
+    new_data['fields']['in_date'] = keyword_list[keyword]['in_date']
+    new_list.append(new_data)
+
+
+with open('new_api/fixtures/keyword_data_final3_result.json', 'w', encoding='UTF-8') as f:
+    json.dump(new_list, f, ensure_ascii=False, indent=2)
--- WordSightProject/wordsight/new_api/models.py
+++ WordSightProject/wordsight/new_api/models.py
@@ -5,7 +5,7 @@ class News(models.Model):
     news_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     origin_address = models.URLField()
     title = models.CharField(max_length=200)
-    created_date = models.DateField()
+    created_date = models.DateField(null=True, blank=True)
     news_agency = models.CharField(max_length=10)
     reporter = models.CharField(max_length=20)
     region = models.CharField(max_length=200, null=True)
@@ -20,9 +20,10 @@ class Keyword(models.Model):
     name = models.CharField(max_length=200)
     count = models.IntegerField()
     in_date = models.CharField(max_length=200)
+
     
-class News_Keyword(models.Model):
+class News_Keyword(models.Model): # pk 자동생성
     news_id = models.ForeignKey(News, on_delete=models.CASCADE, db_column="news_id")
     keyword_id = models.OneToOneField(Keyword, on_delete=models.CASCADE, db_column="keyword_id") # 여러 개 
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200) # 추후 결정 
 
