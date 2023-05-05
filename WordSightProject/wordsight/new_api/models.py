f6d64191d665557dbc6878612939045296f0424b
yunji jo <dbtmxlsk2007@naver.com>
2023-05-05 13:31:30 +0900

    update result json file

--- WordSightProject/wordsight/new_api/models.py
+++ WordSightProject/wordsight/new_api/models.py
@@ -7,23 +7,43 @@ class Keyword(models.Model):
     count = models.IntegerField()
     in_date = models.CharField(max_length=200)
 
+
 class Tag(models.Model):
     tag_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    class1 = models.CharField(default='',null=True, max_length=10) # 대분류
-    class2 = models.CharField(default='',null=True,max_length=10) # 소분류
-    
+    class1 = models.CharField(max_length=20)
+    class2 = models.CharField(max_length=20)
+
 class News(models.Model):
     news_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     origin_address = models.URLField()
     title = models.CharField(max_length=200)
-    created_date = models.DateField(null=True, blank=True)
+    created_date = models.DateField()
     news_agency = models.CharField(max_length=10)
     reporter = models.CharField(max_length=20)
     region = models.CharField(max_length=200, null=True)
     people = models.CharField(max_length=200, null=True)
     company = models.CharField(max_length=200, null=True)
-    tag = models.ManyToManyField(Tag, related_name='related_tag')             
+    tag = models.ManyToManyField(Tag)             
+    tag_list = models.TextField(null=True)
     news_contents = models.TextField()
     image_link = models.URLField()
-    keyword = models.ManyToManyField(Keyword, related_name='related_keyword')
-    keyword_list = models.JSONField(null=True)
+    keyword_list = models.TextField(null=True)
+    keyword = models.ManyToManyField(Keyword)
+
+
+    
+# class News(models.Model):
+#     news_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     origin_address = models.URLField()
+#     title = models.CharField(max_length=200)
+#     created_date = models.DateField(null=True, blank=True)
+#     news_agency = models.CharField(max_length=10)
+#     reporter = models.CharField(max_length=20)
+#     region = models.CharField(max_length=200, null=True)
+#     people = models.CharField(max_length=200, null=True)
+#     company = models.CharField(max_length=200, null=True)
+#     tag = models.ManyToManyField(Tag, related_name='related_tag')             
+#     news_contents = models.TextField()
+#     image_link = models.URLField()
+#     keyword = models.ManyToManyField(Keyword, related_name='related_keyword')
+#     keyword_list = models.JSONField(null=True)
--- WordSightProject/wordsight/new_api/views.py
+++ WordSightProject/wordsight/new_api/views.py
@@ -4,20 +4,19 @@ from .serializers import *
 from .paginators import *
 from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.pagination import PageNumberPagination
 
+class CustomPagination(PageNumberPagination):
+    page_size = 9   
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+    page_query_param = 'page'
 
 # Home:뉴스 목록 9개씩 보여주는 역할 - GET
 class NewsList(generics.ListAPIView):
     queryset = News.objects.all().order_by('-created_date')
     serializer_class = NewsSerializer
     pagination_class = CustomPagination
-    # renderer_classes = [TemplateHTMLRenderer]
-    # template_name = 'newsCard.html'
-    # def get(self, request):
-    #     queryset = News.objects.all().order_by('-created_date')
-    #     # serializer_class = NewsSerializer
-    #     return Response({'news': queryset})
 
 # 뉴스 상세보기 역할
 class NewsDetail(generics.ListAPIView):
