36e2ae337562d90f4a4cf3b2c43c297b02fd196e
TJ Park <hahxowns@naver.com>
2023-05-06 15:39:28 +0900

    Update news filter

--- WordSightProject/wordsight/wordsight_front/views.py
+++ WordSightProject/wordsight/wordsight_front/views.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 from new_api.models import News, Keyword, Tag
+from new_api.models import News, Keyword, Tag
 from new_api.views import NewsList
 from new_api.keyword_analysis import get_relation_keyword
 from django.template.loader import render_to_string
@@ -17,6 +18,9 @@ from new_api.filters import NewsFilter
 def index(request):
     news_list = NewsList.as_view()(request).data['results']
     page_obj = NewsList.as_view()(request).data['page_obj']
+    trend_keyword = Keyword.objects.all().order_by('-count')[:10]
+    context = {'news_list': news_list, 'page_obj': page_obj, 'trend_keyword': trend_keyword }
+
     trend_keyword = Keyword.objects.all().order_by('-count')[:10]
     context = {'news_list': news_list, 'page_obj': page_obj, 'trend_keyword': trend_keyword }
 
@@ -37,33 +41,15 @@ def updateNews(request):
         params["tags"] = params.get("tags", "") + ",".join(request.GET.getlist('tags'))
 
     if request.GET.getlist('agencys'):
-<<<<<<< HEAD
         params["agencys"] = params.get("agencys", "") + ",".join(request.GET.getlist('agencys'))      
     
     test = NewsFilter(params['tags'], params['agencys'])
     news_obj=test.get_news()
 
     print(params)
-    print(news_obj)
+    #print(news_obj)
 
-
-        
-    
     rendered = render_to_string("partial/newsDisplay.html", {"news_list": news_obj})
-=======
-        params["agencys"] = params.get("agencys", "") + ",".join(request.GET.getlist('agencys'))
-    response = requests.get(url, params=params)
-    news_obj = response.json()["results"]
-
-    tag_list = params['tags'].split(',')
-    tag = Tag.objects.filter(class1=tag_list[0])
-    #print(params['tags'].split(','))
-    print(tag[0].news_set.all())
-    #news_obj = tag
-    news_obj = tag[0].news_set.all()
-    
-    rendered = render_to_string("partial/newsDisplay.html", {"news": news_obj})
->>>>>>> main
     return HttpResponse(rendered, content_type="text/plain")
 
 def error404view(request, exception=None):
@@ -71,23 +57,17 @@ def error404view(request, exception=None):
 
 def search(request):
     if request.method == "GET":
-
         keyword = request.GET.get('q')
-
         if keyword:
             relation_keyword = get_relation_keyword(keyword)
             keyword = Keyword.objects.get(name=keyword)
             news = keyword.news_set.all()
             trend_keyword = Keyword.objects.all().order_by('-count')[:10]
-<<<<<<< HEAD
             tag= Tag.objects.none()
             for n in news:
                 tag = tag | n.tag.all()
 
             context= {"keyword": keyword, "relation_keyword":relation_keyword, "news":news, 'trend_keyword':trend_keyword, 'tag':tag }
-=======
-            context= {"keyword": keyword, "relation_keyword":relation_keyword, "news":news, 'trend_keyword':trend_keyword }
->>>>>>> main
 
             if relation_keyword:
                 return render(request, "pages/insight.html", context)
