832729fdbf415c8212da318b16fac2c2bbd58e1e
Dahye K <kangdiamond@gmail.com>
2023-05-07 02:48:23 +0900

    update animation for news card update animation for intro add filter button to hide filter sidebar update filter loading structure

--- WordSightProject/wordsight/wordsight_front/templatetags/temptags.py
+++ WordSightProject/wordsight/wordsight_front/templatetags/temptags.py
@@ -2,10 +2,8 @@ from django import template
 from new_api.models import Keyword
 register = template.Library()
 
-@register.simple_tag
-def getTrend(value):
-    top10key = Keyword.objects.order_by('-count')[:10]
-    return top10key
+def sort_by(queryset, order):
+    return queryset.order_by(order)
 
 @register.filter
 def find_tag(Tag, news_id):
